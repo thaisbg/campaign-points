2c1012d583e458d762df0f38a81bf1ea3a9eaa93
Thais <thsbrgc@gmail.com>
2024-04-13 13:02:09 -0300

    Added comments to explain how was the user table populated

--- src/main/java/com/thaisbg/campaignpoints/tweets/model/User.java
+++ src/main/java/com/thaisbg/campaignpoints/tweets/model/User.java
@@ -4,3 +4,20 @@ public class User {
     private String id;
     private String email;
 }
+
+/*
+To populate the user table, the following script was used in surrealist.app:
+-- Command to create schemafull table
+DEFINE TABLE users SCHEMAFULL;
+
+-- Command to add the email field
+DEFINE FIELD email ON TABLE users TYPE string
+	ASSERT string::is::email($value);
+DEFINE INDEX userEmailIndex ON TABLE users COLUMNS email UNIQUE;
+
+-- Commands to add some users
+CREATE users:nick SET email = 'nick@mail.com';
+CREATE users:david SET email = 'david@mail.com';
+CREATE users:alice SET email = 'alice@mail.com';
+CREATE users:mary SET email = 'mary@mail.com';
+ */
