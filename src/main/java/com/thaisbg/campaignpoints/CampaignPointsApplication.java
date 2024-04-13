2875428a1f0c6584a1c4b20a099020ea6f7a798f
Thais <thsbrgc@gmail.com>
2024-04-13 13:05:16 -0300

    Commiting unversioned files, adding temporal dependency

---
+++ src/main/java/com/thaisbg/campaignpoints/CampaignPointsApplication.java
@@ -0,0 +1,13 @@
+package com.thaisbg.campaignpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignPointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignPointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thaisbg/campaignpoints/CampaignPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thaisbg.campaignpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignPointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
